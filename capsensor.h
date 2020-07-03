3161ea44c0c8418ffbd7c49742c0138d856de087
DCS <dcs@hulilideMacBook-Air.local>
2020-07-03 19:36:56 +0800

    sensorsetting

--- capsensor.h
+++ capsensor.h
@@ -1,23 +1,14 @@
-#include "stm8l15x.h"
-
-
-
 #ifndef __CAPSENSOR_H
 #define __CAPSENSOR_H
 
+#include "stm8l15x.h"
+
+
+#define Cap_Port 		GPIOE //tmp to add
+#define Cap_Charge_Pin 	GPIO_Pin_5
+#define Cap_Detect_Pin	GPIO_Pin_6
+
 extren unsigned char Capchargeflag;
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
