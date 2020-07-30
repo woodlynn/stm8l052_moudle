aab074921098df3b653b572b39db4a3bfc18d18f
DCS <woodlynn@yeah.net>
2020-07-30 10:51:35 +0800

    Add coordinate

---
+++ Display.h
@@ -0,0 +1,14 @@
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
+
+
+#include "stm8l15x.h"
+
+#include "LCD5110.h"
+
+void Display_coordinate (u8 longy,offset);
+
+void Display_tempdot(temp,zerolevel);
+
+#endif
+
