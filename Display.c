c39f989a94a2414b6bc0bafe5e3756451223266e
DCS <woodlynn@yeah.net>
2020-07-30 10:47:38 +0800

    Add coordinate

--- Display.c
+++ Display.c
@@ -13,3 +13,29 @@ void numtochar(unsigned char *strin,*strout,lengh){
 void Display(void){
 
 }
+void Display_coordinate (u8 longy,offset){// the position of x and y
+	u8 x1,x2,y1,y2;
+	y1= LCD_Y- offset;
+	y2=	y1-longy;
+	x1=0;
+	x2=0;
+
+	PutLinetoBuf(x1,y1,x2,y2);
+	x1=0;
+	x2=LCD_x;
+	y1=LCD_Y-offset;
+	y2=y1;
+
+	PutLinetoBuf(x1,y1,x2,y2);
+
+}
+
+void Display_tempdot(temp,zerolevel){
+	static u8 numberoftemp;
+	u8 x,y;
+	x=numberoftemp+1;
+	y= temp/10+zerolevel;
+	PutPointtoBuf(x,y);
+
+
+}
